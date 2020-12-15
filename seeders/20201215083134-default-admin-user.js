ea21220c3f3c309794e0b85fad6dcbba36e9230e
penbillpopo <penbillpopo@gmail.com>
2020-12-15 16:49:36 +0800

    first commit

---
+++ seeders/20201215083134-default-admin-user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('users', [{
+      name: 'Bill',
+      email: 'penbillpopo@gmail.com',
+      password: '1234qwer',
+      sex: 'male'
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
